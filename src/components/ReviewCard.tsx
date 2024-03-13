c831c8e4d2fb11fdf92851467960dd40c3d50123
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-03-13 15:33:43 -0600

    fix card styling

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -5,7 +5,7 @@ import StarRating from './StarRating';
 
 export default function ReviewCard({ review }: any) {
   return (
-    <div key={review.id.label} className="bg-[#f8f8f8] rounded-md p-4">
+    <div key={review.id.label} className="bg-[#f8f8f8] rounded-md p-4 text-xs">
       <ReactStars
         value={Number(review['im:rating'].label)}
         isEdit={false}
@@ -14,7 +14,7 @@ export default function ReviewCard({ review }: any) {
         filledIcon={<StarRating color="#ffac33" />}
         emptyIcon={<StarRating color="#BFBFBF" />}
       />
-      <div className="text-gray-500 text-xs flex flex-row ">
+      <div className="text-gray-500 text-xs flex flex-row mb-4">
         <p>{review.author.name.label}</p>,
         <p className="ml-1">{moment(review.updated.label).format('l')}</p>
       </div>
