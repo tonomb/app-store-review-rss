0ff7c40ac561242549f59a4519d80f800462f371
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-03-13 15:29:16 -0600

    adds star ratings to review card

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import moment from 'moment';
+import ReactStars from 'react-rating-star-with-type';
+import StarRating from './StarRating';
 
 export default function ReviewCard({ review }: any) {
   return (
     <div key={review.id.label} className="bg-[#f8f8f8] rounded-md p-4">
-      <p>{review['im:rating'].label}</p>
+      <ReactStars
+        value={Number(review['im:rating'].label)}
+        isEdit={false}
+        size={'25px'}
+        classNames="mb-2"
+        filledIcon={<StarRating color="#ffac33" />}
+        emptyIcon={<StarRating color="#BFBFBF" />}
+      />
       <div className="text-gray-500 text-xs flex flex-row ">
         <p>{review.author.name.label}</p>,
         <p className="ml-1">{moment(review.updated.label).format('l')}</p>
